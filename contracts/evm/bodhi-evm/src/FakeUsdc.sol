29e0f38cdfa47c89c03c76a894bb4af8716090da
axatbhardwaj <axatbhardwaj@outlook.com>
2024-10-12 23:41:49 +0530

    added decimals in minting

--- contracts/evm/bodhi-evm/src/FakeUsdc.sol
+++ contracts/evm/bodhi-evm/src/FakeUsdc.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract USDC is ERC20, Ownable {
-    uint256 private _totalSupply = 20000 * decimals();
+    uint256 private _totalSupply = 2000000 * 10 ** 18;
 
     constructor() ERC20("FUSDC", "FUSD") Ownable(msg.sender) {
         _mint(msg.sender, _totalSupply);
