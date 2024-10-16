2aa649836be64cd87102d18ccf6a22660aca2723
Supriya Gorai <supriyagorai1995@gmail.com>
2024-10-16 15:11:22 +0530

    update topup page design

--- apps/web/app/topup/page.tsx
+++ apps/web/app/topup/page.tsx
@@ -6,7 +6,7 @@ export const dynamic = "force-dynamic";
 export default async function Page() {
   await checkAuth();
   return (
-    <div className="flex flex-col min-h-screen dark:bg-gradient-to-r dark:from-primary-900 dark:via-primary-800  dark:to-gray-800 text-gray-900 dark:text-gray-100 transition-colors duration-500">
+    <div className="flex flex-col min-h-screen bg-gray-200 dark:bg-gradient-to-r dark:from-primary-900 dark:via-primary-800  dark:to-gray-800 text-gray-900 dark:text-gray-100 transition-colors duration-500">
       <Topup />
     </div>
   );
